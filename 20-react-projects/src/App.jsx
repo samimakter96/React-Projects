7a0af068450266fb4302efc356d667513156791d
samimakter96 <samimakter9641@gmail.com>
2024-05-07 17:53:55 +0530

    load-more-data, qr-code-generator, tree-view

--- 20-react-projects/src/App.jsx
+++ 20-react-projects/src/App.jsx
@@ -3,6 +3,10 @@ import Accordion from './Components/accordion';
 import RandomColor from './Components/random-color';
 import StarRating from './Components/star-rating';
 import ImageSlider from './Components/image-slider';
+import LoadMoreData from './Components/load-more-data';
+import TreeView from './Components/tree-view';
+import menus from './Components/tree-view/data';
+import QRCodeGenerator from './Components/qr-code-generator';
 
 function App() {
   return (
@@ -17,11 +21,20 @@ function App() {
       {/* <StarRating noOfStarts={10} /> */}
 
       {/* Image slider component */}
-      <ImageSlider
+      {/* <ImageSlider
         url={'https://picsum.photos/v2/list'}
         page={'1'}
         limit={'10'}
-      />
+      /> */}
+
+      {/* Load more data */}
+      {/* <LoadMoreData /> */}
+
+      {/* Tree view component / menu UI component / recursive  navigation menu */}
+      {/* <TreeView menus={menus}/> */}
+
+      {/* QR Code Generator */}
+      <QRCodeGenerator />
     </div>
   );
 }
